889ddec7dc9547ff55b3e2a70d7f23ef62844351
Ethan Strauss <estrauss@gmail.com>
2025-01-29 20:33:06 -0400

    feat: First 8 sections

--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,17 @@
-const path = require('path');
-
 const express = require('express');
 
-const rootDir = require('../utilities/path');
+const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'))
-})
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/checkout', shopController.getCheckout);
+
+router.get('/orders', shopController.getOrders);
 
 module.exports = router;
